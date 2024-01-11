62def05134b3eba23f60f399bf6acfdf22701a28
klu-2200090053 <2200090053@kluniversity.in>
2024-01-11 14:04:56 +0530

    This is version1 file

---
+++ add.py
@@ -0,0 +1,6 @@
+This is a python file
+int sum1,sum2,sum3;
+scanf &sum1,&sum2
+printf enter the values of sum1 and sum2
+sum[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C = sum1 + sum2
+printf sum
